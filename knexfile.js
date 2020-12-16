dd24540e682abaed829f676599e38c591b427086
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-12-16 18:12:26 -0300

    chore: create knex config

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST, 
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/app/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/app/database/seeds`,
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
